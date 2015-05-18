3cf5880c6e9b8705aa0860718ec58c99f7540d11
budel <daniel.budelmann@student.jade-hs.de>
2015-05-18 19:20:20 +0200

    first commit: Empty GUI with mouse callback

---
+++ superSinusGUI.m
@@ -0,0 +1,34 @@
+function superSinusGUI
+% Function to show GUI which controls 2d sin waves
+% Author: Daniel Budelmann and Sebastian Voges (c) IHA @ Jade Hochschule applied licence see EOF 
+% Version History:
+% Ver. 0.01 initial create 18-May-2015 			 Initials DB and SV
+
+% create empty image and show it
+imObj = ones(500,500);
+figure;
+imageHandle = imshow(imObj);
+% attach mouseclick callback
+set(imageHandle,'ButtonDownFcn',@onMouseClick);
+
+
+%--------------------Licence ---------------------------------------------
+% Copyright (c) <2015> Daniel Budelmann and Sebastian Voges
+% Institute for Hearing Technology and Audiology
+% Jade University of Applied Sciences 
+% Permission is hereby granted, free of charge, to any person obtaining 
+% a copy of this software and associated documentation files 
+% (the "Software"), to deal in the Software without restriction, including 
+% without limitation the rights to use, copy, modify, merge, publish, 
+% distribute, sublicense, and/or sell copies of the Software, and to
+% permit persons to whom the Software is furnished to do so, subject
+% to the following conditions:
+% The above copyright notice and this permission notice shall be included 
+% in all copies or substantial portions of the Software.
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
